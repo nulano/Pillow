ccd4ceb70f3c7b7848614639444c2ea3228a6a77
Andrew Murray <radarhere@users.noreply.github.com>
2019-08-03 21:26:32 +1000

    Load User32 after possible return

--- src/display.c
+++ src/display.c
@@ -332,8 +332,8 @@ PyImaging_GrabScreenWin32(PyObject* self, PyObject* args)
     DWORD rop;
     PyObject* buffer;
     HANDLE dpiAwareness;
-	HMODULE user32 = LoadLibraryA("User32.dll");
-	Func_SetThreadDpiAwarenessContext SetThreadDpiAwarenessContext_function;
+    HMODULE user32;
+    Func_SetThreadDpiAwarenessContext SetThreadDpiAwarenessContext_function;
 
     if (!PyArg_ParseTuple(args, "|i", &includeLayeredWindows))
         return NULL;
@@ -346,6 +346,7 @@ PyImaging_GrabScreenWin32(PyObject* self, PyObject* args)
 
     // added in Windows 10 (1607)
     // loaded dynamically to avoid link errors
+    user32 = LoadLibraryA("User32.dll");
     SetThreadDpiAwarenessContext_function =
             (Func_SetThreadDpiAwarenessContext)
             GetProcAddress(user32, "SetThreadDpiAwarenessContext");
