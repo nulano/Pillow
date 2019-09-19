5c3577d2f2d1c17ce42e80d1f66ba729ab424080
nulano <nulano@nulano.eu>
2019-09-20 01:14:35 +0200

    xcb screengrab fixes

--- Tests/test_imagegrab.py
+++ Tests/test_imagegrab.py
@@ -1,62 +1,65 @@
 import subprocess
 import sys
 
-from .helper import PillowTestCase
+from PIL import Image, ImageGrab
 
-try:
-    from PIL import ImageGrab
+from .helper import PillowTestCase, unittest
 
-    class TestImageGrab(PillowTestCase):
-        def test_grab(self):
-            for im in [
-                ImageGrab.grab(),
-                ImageGrab.grab(include_layered_windows=True),
-                ImageGrab.grab(all_screens=True),
-            ]:
-                self.assert_image(im, im.mode, im.size)
 
-        def test_grabclipboard(self):
-            if sys.platform == "darwin":
-                subprocess.call(["screencapture", "-cx"])
-            else:
-                p = subprocess.Popen(
-                    ["powershell", "-command", "-"], stdin=subprocess.PIPE
-                )
-                p.stdin.write(
-                    b"""[Reflection.Assembly]::LoadWithPartialName("System.Drawing")
-[Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
-$bmp = New-Object Drawing.Bitmap 200, 200
-[Windows.Forms.Clipboard]::SetImage($bmp)"""
-                )
-                p.communicate()
-
-            im = ImageGrab.grabclipboard()
+class TestImageGrab(PillowTestCase):
+    @unittest.skipUnless(
+        sys.platform in ("win32", "darwin"), "requires Windows or macOS"
+    )
+    def test_grab(self):
+        for im in [
+            ImageGrab.grab(),
+            ImageGrab.grab(include_layered_windows=True),
+            ImageGrab.grab(all_screens=True),
+        ]:
             self.assert_image(im, im.mode, im.size)
 
+    @unittest.skipUnless(Image.core.HAVE_XCB, "requires XCB")
+    def test_grab_x11(self):
+        try:
+            if sys.platform not in ("win32", "darwin"):
+                im = ImageGrab.grab()
+                self.assert_image(im, im.mode, im.size)
 
-except ImportError:
+            im2 = ImageGrab.grab(xdisplay="")
+            self.assert_image(im2, im2.mode, im2.size)
+        except IOError as e:
+            self.skipTest(str(e))
 
-    class TestImageGrab(PillowTestCase):
-        def test_skip(self):
-            self.skipTest("ImportError")
-
-
-class TestImageGrabImport(PillowTestCase):
-    def test_import(self):
-        # Arrange
+    @unittest.skipUnless(Image.core.HAVE_XCB, "requires XCB")
+    def test_grab_invalid_xdisplay(self):
         exception = None
 
-        # Act
         try:
-            from PIL import ImageGrab
-
-            ImageGrab.__name__  # dummy to prevent Pyflakes warning
+            ImageGrab.grab(xdisplay="error.test:0.0")
         except Exception as e:
             exception = e
 
-        # Assert
-        if sys.platform in ["win32", "darwin"]:
-            self.assertIsNone(exception)
+        self.assertIsInstance(exception, IOError)
+        self.assertTrue(str(exception).startswith("X connection failed"))
+
+    @unittest.skipUnless(
+        sys.platform in ("win32", "darwin"), "requires Windows or macOS"
+    )
+    def test_grabclipboard(self):
+        if sys.platform == "darwin":
+            subprocess.call(["screencapture", "-cx"])
+        elif sys.platform == "win32":
+            p = subprocess.Popen(["powershell", "-command", "-"], stdin=subprocess.PIPE)
+            p.stdin.write(
+                b"""[Reflection.Assembly]::LoadWithPartialName("System.Drawing")
+[Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+$bmp = New-Object Drawing.Bitmap 200, 200
+[Windows.Forms.Clipboard]::SetImage($bmp)"""
+            )
+            p.communicate()
         else:
-            self.assertIsInstance(exception, ImportError)
-            self.assertEqual(str(exception), "ImageGrab is macOS and Windows only")
+            self.skipTest("ImageGrab.grabclipboard() is macOS and Windows only")
+            return
+
+        im = ImageGrab.grabclipboard()
+        self.assert_image(im, im.mode, im.size)
