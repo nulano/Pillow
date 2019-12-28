d82c0eae88350948fce9a83237e7ae824b74eb3f
nulano <nulano@nulano.eu>
2019-12-28 12:01:19 +0100

    Use NotImplementedError in ImageGrab if using Linux
    Co-Authored-By: Andrew Murray <3112309+radarhere@users.noreply.github.com>

--- src/PIL/ImageGrab.py
+++ src/PIL/ImageGrab.py
@@ -99,4 +99,4 @@ def grabclipboard():
             return BmpImagePlugin.DibImageFile(io.BytesIO(data))
         return data
     else:
-        raise IOError("ImageGrab.grabclipboard() is macOS and Windows only")
+        raise NotImplementedError("ImageGrab.grabclipboard() is macOS and Windows only")
