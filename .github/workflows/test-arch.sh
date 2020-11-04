62efbec7a3ccef4ede5a81ea23151e2ab044a52f
nulano <nulano@nulano.eu>
2020-11-04 02:10:06 +0000

    fix coverage

--- .github/workflows/test-arch.sh
+++ .github/workflows/test-arch.sh
@@ -5,10 +5,11 @@ run_in_docker() {
     cat <(echo "cd /pillow") $1 > $target
     chmod +x $target
     echo "*** Starting docker ***"
-    docker run --name runner -v $(pwd):/pillow container:latest bash -eo pipefail /pillow/$target
+    docker run --name runner -v $(pwd):/pillow ${@:2} container:latest bash -eo pipefail /pillow/$target
     echo "*** Saving docker session ***"
     docker commit runner container:latest
     docker rm runner
 }
 
-run_in_docker $1
+# test-arch.yml will append a line in the following format:
+# run_in_docker $1 -e GITHUB_ACTIONS=true -e GITHUB_SHA=abcdef ...
