b703f9e18b4c1bfa6870bf8f411cf4aee1e7fc1e
Nulano <ondreko.tiba@gmail.com>
2020-11-15 23:09:46 +0100

    fix raqm_cmake

--- depends/install_raqm_cmake.sh
+++ depends/install_raqm_cmake.sh
@@ -3,10 +3,11 @@
 
 
 archive=raqm-cmake-99300ff3
+directory=libraqm-cmake-99300ff3dd408b8593e321fa2c62df97d74ff128
 
 ./download-and-extract.sh $archive https://raw.githubusercontent.com/python-pillow/pillow-depends/master/$archive.tar.gz
 
-pushd $archive
+pushd $directory
 
 mkdir build
 cd build
