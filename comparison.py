ac82d2609ad5e32540b935914a5aa0209ac573f8
nulano <nulano@nulano.eu>
2022-10-02 08:00:03 +0200

    print features, disable heap verification

--- comparison.py
+++ comparison.py
@@ -1,6 +1,11 @@
 import time
+
+from PIL import features
+
 start = time.time()
 i = 0
 while i < 100000000:
     i += 1
 print("Comparison:", time.time() - start)
+
+features.pilinfo(supported_formats=False)
