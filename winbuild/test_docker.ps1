453b4cbd9e9bb9564589a174604325336e376665
Nulano <nulano@nulano.eu>
2023-10-09 22:06:47 +0200

    test

--- winbuild/test_docker.ps1
+++ winbuild/test_docker.ps1
@@ -1,17 +1,18 @@
 param ([string]$python)
 $ErrorActionPreference  = 'Stop'
 $ProgressPreference = 'SilentlyContinue'
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 if ($python -like "pypy*") {
-    $url = 'https://downloads.python.org/pypy/{0}-v7.3.13-win64.zip' -f $python
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
     Invoke-WebRequest -Uri 'https://aka.ms/vs/15/release/vc_redist.x64.exe' -OutFile 'vc_redist.x64.exe'
     & C:\vc_redist.x64.exe /install /quiet /norestart
+    $url = 'https://downloads.python.org/pypy/{0}-v7.3.13-win64.zip' -f $python
+    echo "Downloading $url"
     Invoke-WebRequest -Uri $url -OutFile 'pypy.zip'
     tar -xf 'pypy.zip'
     mv pypy*-win64 C:\Python
 } else {
     $url = 'https://www.python.org/ftp/python/{0}.0/python-{0}.0-amd64.exe' -f $python
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    echo "Downloading $url"
     Invoke-WebRequest -Uri $url -OutFile 'setup.exe'
     Start-Process setup.exe -Wait -NoNewWindow -PassThru -ArgumentList @(
             '/quiet',
@@ -27,6 +28,7 @@ if ($python -like "pypy*") {
 $env:CI = "true"
 $env:path += ";C:\Python\;C:\pillow\winbuild\build\bin\"
 cd C:\pillow
+echo "Running test"
 & python -VV
 & python -m ensurepip
 & python -m pip install -U pip
